80fc5190de7a3d049dcb303eb6631b76cb53a649
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-02-25 21:00:49 +0300

    Comments added

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,9 @@
 public class CreditPaymentService {
     public int calculate(int amount, int term, double rate) {
-        double monthlyRate = rate / 12 / 100;
-        double montlyRateInDegree = Math.pow((monthlyRate + 1), term);
+        double monthlyRate = rate / 12 / 100; // Переменная для хранения месячной процентной ставки
+        double montlyRateInDegree = Math.pow((monthlyRate + 1), term); // Месячную процентную ставку возводим в степень
+
+        // Формула расчета аннуитетного платежа по кредиту:
         int result = (int) ((int) amount * monthlyRate * montlyRateInDegree / (montlyRateInDegree - 1));
         return result;
     }
